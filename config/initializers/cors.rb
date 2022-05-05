8bf741399e0c53974368ebb66758b7f187921ab4
Caitlyn Dalton <cdalton0610@gmail.com>
2022-05-04 22:24:14 -0400

    Added frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "mini-capstone.peterxjang.com/restful-routes/products"
+    origins "mini-capstone.peterxjang.com"
 
     resource "*",
       headers: :any,
