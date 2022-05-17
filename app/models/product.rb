a6598f6eaf4d8a1d3c5c83224dbb9911078d673f
Caitlyn Dalton <cdalton0610@gmail.com>
2022-05-17 19:17:02 -0400

    Add category model associated w products through join table

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,8 @@ class Product < ApplicationRecord
 
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
