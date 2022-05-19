20d706fb55ff555f2c9bc327301e691d69dc11be
Caitlyn Dalton <cdalton0610@gmail.com>
2022-05-18 21:30:45 -0400

    Fixed cartedproducts controller actions

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,24 +1,24 @@
 class CartedProductsController < ApplicationController
   def index
-    @carted_products = CartedProduct.all
-    render json: @carted_products.as_json
+    carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
+    render json: carted_products.as_json
   end
 
   def create
-    @carted_product = CartedProduct.new(
-      user_id: params[:user_id],
+    carted_product = CartedProduct.new(
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
-      order_id: params[:order_id],
+      status: "carted",
+      order_id: nil,
     )
-    @carted_product.save
+    carted_product.save
     render json: @carted_product.as_json
   end
 
   def destroy
-    @carted_product = CartedProduct.find_by(id: params["id"])
-    @carted_product.destroy
+    carted_product = CartedProduct.find_by(id: params["id"])
+    carted_product.destroy
     render json: { message: "Cart deleted successfully." }
   end
 end
