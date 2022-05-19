b4adc65fe406edf6e3e59191a79a6f14d5f47d13
Caitlyn Dalton <cdalton0610@gmail.com>
2022-05-18 22:32:14 -0400

    Fixed errors and customized json for frontend

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,24 +1,29 @@
 class CartedProductsController < ApplicationController
   def index
-    carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
-    render json: carted_products.as_json
+    @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
+    render template: "carted_products/index"
   end
 
   def create
-    carted_product = CartedProduct.new(
+    @carted_product = CartedProduct.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
       status: "carted",
       order_id: nil,
     )
-    carted_product.save
-    render json: @carted_product.as_json
+    @carted_product.save
+    render template: "carted_products/show"
   end
 
   def destroy
-    carted_product = CartedProduct.find_by(id: params["id"])
-    carted_product.destroy
-    render json: { message: "Cart deleted successfully." }
+    carted_product = CartedProduct.find_by(id: params[:id], user_id: current_user.id, status: "carted")
+    if carted_product
+      carted_product.status = "removed"
+      carted_product.save
+      render json: { message: "Carted product successfully deleted." }
+    else
+      render json: { message: "Invalid request" }, status: 422
+    end
   end
 end
