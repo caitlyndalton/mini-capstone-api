13be83ac3e2ecddd80e138d8b63f150cf374f8dd
Caitlyn Dalton <cdalton0610@gmail.com>
2022-06-14 20:19:14 -0400

    Revised seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: "Cait Dalton", email: "disconerds@gmail.com", password: "password", admin: true)
+
 Supplier.create(name: "Shein", email: "help@shein.com", phone_number: "8008675309")
 Supplier.create(name: "Lirika Matoshi", email: "help@lirikamatoshi.com", phone_number: "4048675309")
 Supplier.create(name: "Tetua Matoshi", email: "info@tetuamatoshi.com", phone_number: "9176348522")
